935c32d7d33f1e7f9c7a137cb0b31556034056c1
Ali Khan Afridi <afridi2@illinois.edu>
2014-02-14 20:45:20 -0500

    Added some global variables and Bing Translation Library

--- src/com/example/link/Globals.java
+++ src/com/example/link/Globals.java
@@ -1,5 +1,33 @@
 package com.example.link;
 
+import com.memetix.mst.language.Language;
+
 public class Globals {
 	// List of static variables
+	// Static variables that need to be saved:
+	
+		// Number of translations the user has left
+		public static volatile int creditsRemaining; 
+
+		// Number of translations used
+		public static volatile int creditsUsed; 
+		
+		// Whether or not a coupon has been used
+		public static volatile boolean couponUsed;
+		
+		// Whether this is the first time the app is being opened
+		public static volatile boolean firstOpen = true;
+		public static volatile int initialCredits = 5;
+		
+		// Whether or not the account has been upgraded to premium
+		public static volatile boolean premiumAccount;
+		
+		// Whether or not the user has shared or rated the app
+		public static volatile boolean appSharedFb;
+		public static volatile boolean appTweeted;
+		public static volatile boolean appRated;
+		
+		
+		public static volatile Language defaultLanguage1;
+		public static volatile Language defaultLanguage2;
 }
